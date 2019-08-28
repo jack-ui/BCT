1f1380b0f5ba815289d7cace5583f60a1e216246
EmilieGitHub <emilie.jean.courriel@gmail.com>
2019-08-28 14:40:56 +0200

    rectif usercontrolle from emilie

---
+++ src/Controller/UserController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request; 
+use Symfony\Component\HttpFoundation\Response; 
+
+use App\Form\UserType;
+use App\Entity\User;  
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserController extends AbstractController
+{
+	
+	
+	/**
+	* @Route("/inscription", name="inscription")
+	*
+	*/
+	public function inscription(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder){
+		
+		$user = new User; 
+		$form = $this -> createForm(UserType::class, $user);
+		
+		// traiter les infos du formulaire
+		$form -> handleRequest($request);
+		if($form -> isSubmitted() && $form -> isValid()){
+			$manager -> persist($user);
+			
+			
+			if($user -> getDateDeNaissance() -> format('Y') > date('Y') - 16 ){
+				// si '2015' supérieur ('2019' - 16 = 2003)
+				$this -> addFlash('errors', 'trop jeune');
+				return $this -> redirectToRoute('accueil');
+			}
+			
+			
+			$user -> setRole('ROLE_USER'); 
+			
+			// Mdp saisi dans le formulaire :
+			$password = $user -> getPassword(); 
+			
+			// on encode selon l'algo choisi dans security.yaml pour cette entité 
+			$password_crypte = $encoder -> encodePassword($user, $password);
+			$user -> setPassword($password_crypte);
+			
+			$manager -> flush();
+			
+			$this -> addFlash('success', 'Félicitations, votre inscription a bien été prise en compte !');
+			return $this -> redirectToRoute('connexion');
+		}
+		
+		return $this -> render('user/register.html.twig', [
+			'userForm' => $form -> createView()
+		]);
+	}
+	
+	
+}
+
