dd22ca5d880f0fd7242c1b7a537351c97909ea88
Etudiant <bldlr170289@gmail.com>
2019-08-28 15:31:37 +0200

    modif usertype pour validation inscription

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,30 +6,20 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType; 
-use Symfony\Component\Form\Extension\Core\Type\PasswordType; 
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType; 
-use Symfony\Component\Form\Extension\Core\Type\BirthdayType; 
-use Symfony\Component\Form\Extension\Core\Type\TextType; 
-use Symfony\Component\Form\Extension\Core\Type\TextareaType; 
-use Symfony\Component\Form\Extension\Core\Type\IntegerType; 
-use Symfony\Component\Form\Extension\Core\Type\EmailType; 
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-<<<<<<< HEAD
-        ->add('username')
-        ->add('email')
-        ->add('password')
-        ->add('nom')
-        ->add('prenom')
-        ->add('dateNaissance')
-        ->add('sexe')
-        ->add('adresse')
-=======
             -> add('username', TextType::class)
             -> add('email', EmailType::class)
             -> add('prenom', TextType::class)
@@ -47,11 +37,10 @@ class UserType extends AbstractType
             -> add('telephone', TextType::class)
             -> add('dateDeNaissance', BirthdayType::class)
 			-> add('submit', SubmitType::class)
->>>>>>> c3ff98e8bd4c9d1eaffb1ff96c3e30e72f5f2f49
         ;
-		
-		
-		if($options['admin'] == true){	
+
+
+		if($options['admin'] == true){
 			$builder -> add('role', ChoiceType::class, array(
 				'choices' => array(
 					'Client' => 'ROLE_USER',
