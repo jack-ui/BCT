cc9061d03c9f9872de991b316ad1a6e68e4daf3b
Magali Milbergue <magali.milbergue@gmail.com>
2019-08-30 15:19:10 +0200

    correction role dans entity user Mag

---
+++ src/Migrations/Version20190830131726.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190830131726 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE commande DROP reference');
+        $this->addSql('ALTER TABLE user ADD role VARCHAR(20) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE commande ADD reference VARCHAR(5) NOT NULL COLLATE utf8mb4_unicode_ci');
+        $this->addSql('ALTER TABLE user DROP role');
+    }
+}
