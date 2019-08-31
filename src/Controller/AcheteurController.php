ed291332f4c960739bf4447f3911ada88f4576bd
EmilieGitHub <emilie.jean.courriel@gmail.com>
2019-08-31 16:42:24 +0200

    fonction showProfile by emi

--- src/Controller/AcheteurController.php
+++ src/Controller/AcheteurController.php
@@ -14,28 +14,40 @@ class AcheteurController extends AbstractController
 	* @Route("/search", name="search")
 	*
     */
-
     public function search()
-    {
-        //la page avec la barre de recherche
+    {   
+        //Fonction qui retourne un affichage
+        //Affiche la page avec la barre de recherche
         return $this -> render('acheteur/recherche.html.twig', [
             ]);
-
-            //dirige vers les résultats sous forme de liste
+       
     }
+    //test : localhost:8000/search
 
 
     /**
 	* @Route("/search_results", name="search_results")
 	*
     */
-
     public function searchResults()
     {
-        //la page avec le résultat de la recherche sous forme de liste de boutiques 
+        //Fonction pour afficher les boutiques les plus proches qui correspondent au résultat demandé
+        //La recherche se fait en fonction de $term (à mettre en argument et sur la route)
+
+        //Ex recherche par code postal (non par boutique car l'acheteur ne connait pas le nom des boutiques)
+        //SELECT localisation FROM boutique WHERE localisation LIKE '95%'
+
+        //Recherche par produit ici tomate
+        //SELECT nom FROM produit WHERE nom = tomate
+        //entre  
+
+
+        
+
         return $this -> render('acheteur/search_results.html.twig', [
             ]);
     } 
+    //test : localhost:8000/search_results
     
 
 
@@ -43,8 +55,7 @@ class AcheteurController extends AbstractController
 	* @Route("/cart", name="cart")
 	*
     */
-
-    public function cart()
+    public function addToCart()
     {
         return $this -> redirectToRoute('confirmation'); //si la commande est validée, on va vers la page confirmation
     } ///VOIR POUR LE PANIER, ON NE L'A PAS FAIT EN COURS
